7e1f4b4f1fdf6693646659467866b811886853ba
opeye-sayani <sayani@opaai.in>
2026-07-22 19:45:14 +0530

    Changes to rollback

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "apps.authentication.apps.AuthenticationConfig",  # Add your authentication app here
+    
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("apps.authentication.api.urls")),  # Include the authentication app's API URLs
 ]
+
