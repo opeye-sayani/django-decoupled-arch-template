e397e6b4975d8d0512b129bc40c57cc6d5108ecb
opeye-sayani <sayani@opaai.in>
2026-07-22 20:44:06 +0530

    Revert "My feature branch"

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
-    "apps.authentication.apps.AuthenticationConfig",  # Add your authentication app here
-    
 ]
 
 MIDDLEWARE = [
